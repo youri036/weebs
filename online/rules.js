bf22b2912cfc414c30908085172c45ecca22568a
youri036 <youriyouri036@gmail.com>
2024-02-15 13:34:32 +0100

    update

---
+++ online/rules.js
@@ -0,0 +1,13 @@
+function cbd(){
+    window.location.href = 'cbd.html';
+}
+function cbdr(){
+    window.location.href = 'cbdm.html';
+}
+function cbdms(){
+    window.location.href = 'cbdm.html';
+}
+
+function cbdm(){
+    window.location.href = 'cbdm.html';
+}
